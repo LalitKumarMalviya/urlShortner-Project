de3af4f94610e2de6f821df9fa930c2fec90fe49
lalit <lk2690722@gmail.com>
2022-12-04 00:26:59 +0530

    Impliment Redis chaching and Using two commands SETEX & GET

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,8 +1,35 @@
 const urlModel = require('../model/urlModel')
 const validUrl = require('valid-url')
 const shortId = require('shortid')
+const redis = require('redis')
+const { promisify } = require('util')
 
 const baseUrl = 'localhost:5000'
+
+//-------------------------------{ connection for Redis }----------------------------------//
+
+//1.Connect to redis
+const redisClient = redis.createClient(
+    11616,   //port
+    "redis-11616.c264.ap-south-1-1.ec2.cloud.redislabs.com",  //public endpoint
+    { no_ready_check: true }
+);
+redisClient.auth("TOatNLwfyiUlzrWAIg9XCYa41Slt46bE", function (err) {  //password
+    if (err) throw err;
+});
+
+
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis...");
+});
+
+//2. Use the Commands
+
+const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient)
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient)
+
+
+//--------------------------------{ create Short Url }-------------------------------------//
 
 const urlShortner = async function (req, res) {
     try {
@@ -16,7 +43,7 @@ const urlShortner = async function (req, res) {
         url = url.trim()
 
         if (!validUrl.isWebUri(url)) {
-            return res.status(400).send({ status: false, message: 'Input Url is Invalid!' })
+            return res.status(400).send({ status: false, message: 'Url is Invalid!' })
         }
 
         let checkedUrl = await urlModel.findOne({ longUrl: url }).select({ _id: 0, __v: 0 })
@@ -34,6 +61,8 @@ const urlShortner = async function (req, res) {
             urlCode: urlCode
         })
 
+        //set the data in redis for 1hr...
+        await SET_ASYNC(`${urlCode}`, (60 * 60), JSON.stringify(savedData))
         let savedData1 = await urlModel.findById(savedData._id).select({ _id: 0, __v: 0 })
 
         return res.status(201).send({ status: true, data: savedData1 })
@@ -44,19 +73,28 @@ const urlShortner = async function (req, res) {
     }
 }
 
+//--------------------------{ Redirect Short Url => Main Url }-----------------------------//
 
 const redirect = async function (req, res) {
     try {
         let urlCode = req.params.urlCode
 
-        let urlData = await urlModel.findOne({ urlCode: urlCode })
-        if (!urlData) {
-            return res.status(404).send({ status: false, message: 'Url Not Found!' })
+        let cachedData = await GET_ASYNC(`${urlCode}`)
+
+        if (cachedData) {
+            let data = JSON.parse(cachedData)
+            return res.redirect(302, data.longUrl)
+
+        } else {
+            let urlData = await urlModel.findOne({ urlCode: urlCode })
+            if (!urlData) {
+                return res.status(404).send({ status: false, message: 'Url Not Found!' })
+            }
+
+            await SET_ASYNC(`${urlCode}`, (60 * 60), JSON.stringify(urlData))
+            let longUrl = urlData.longUrl
+            return res.redirect(302, longUrl)
         }
-
-        let longUrl = urlData.longUrl
-
-        return res.redirect(302, longUrl)
     }
 
     catch (err) {
